b807bde8782c2a387116f4a133accf3022feb191
FarmFarmRakpong <farmza560@gmail.com>
2023-02-15 20:58:52 +0700

    add controller

---
+++ controller/product/product.go
@@ -0,0 +1,59 @@
+package product
+
+import (
+	"strconv"
+
+	"github.com/labstack/echo/v4"
+)
+
+type Movie struct {
+	ImdbID string `json:"imdbID"`
+	Title  string `json:"title"`
+	Year   int    `json:"year"`
+}
+
+var movies = []Movie{
+	{
+		ImdbID: "tt0111161",
+		Title:  "The Shawshank Redemption",
+		Year:   1994,
+	},
+}
+
+func GetAllMoviesHandler(c echo.Context) error {
+	y := c.QueryParam("year")
+
+	if y == "" {
+		return c.JSON(200, movies)
+	}
+	year, err := strconv.Atoi(y)
+	if err != nil {
+		return c.JSON(400, err.Error())
+	}
+	ms := []Movie{}
+	for _, m := range movies {
+		if m.Year == year {
+			ms = append(ms, m)
+		}
+	}
+	return c.JSON(200, ms)
+}
+func GetAllMoviesByIDHandlerByID(c echo.Context) error {
+	id := c.Param("id")
+	// fmt.Println(id)
+	for _, m := range movies {
+		if m.ImdbID == id {
+			return c.JSON(200, m)
+		}
+	}
+	return c.JSON(200, map[string]string{"message": "movie" + id})
+}
+func CreateMovies(c echo.Context) error {
+	m := &Movie{}
+
+	if err := c.Bind(m); err != nil {
+		return c.JSON(400, err.Error())
+	}
+	movies = append(movies, *m)
+	return c.JSON(200, m)
+}
--- routes/Route.go
+++ routes/Route.go
@@ -1,20 +1,19 @@
 package routes
 
 import (
-	"strconv"
-
+	"github.com/farmrakpong/goimdbreal/controller/product"
 	"github.com/labstack/echo/v4"
 )
 
 func UserRoute(e *echo.Echo) {
 	// Define the route for getting all users
-	e.GET("/movies", getAllMoviesHandler)
+	e.GET("/movies", product.GetAllMoviesHandler)
 
 	// // Define the route for getting a user by ID
-	e.GET("/movies/:id", getAllMoviesByIDHandlerByID)
+	e.GET("/movies/:id", product.GetAllMoviesByIDHandlerByID)
 
 	// // Define the route for creating a new user
-	e.POST("/users", createMovies)
+	e.POST("/users", product.CreateMovies)
 
 	// // Define the route for updating a user by ID
 	// e.PUT("/users/:id", updateUser)
@@ -22,75 +21,3 @@ func UserRoute(e *echo.Echo) {
 	// // Define the route for deleting a user by ID
 	// e.DELETE("/users/:id", deleteUser)
 }
-
-type Movie struct {
-	ImdbID string `json:"imdbID"`
-	Title  string `json:"title"`
-	Year   int    `json:"year"`
-}
-
-var movies = []Movie{
-	{
-		ImdbID: "tt0111161",
-		Title:  "The Shawshank Redemption",
-		Year:   1994,
-	},
-}
-
-func getAllMoviesHandler(c echo.Context) error {
-	y := c.QueryParam("year")
-
-	if y == "" {
-		return c.JSON(200, movies)
-	}
-	year, err := strconv.Atoi(y)
-	if err != nil {
-		return c.JSON(400, err.Error())
-	}
-	ms := []Movie{}
-	for _, m := range movies {
-		if m.Year == year {
-			ms = append(ms, m)
-		}
-	}
-	return c.JSON(200, ms)
-}
-func getAllMoviesByIDHandlerByID(c echo.Context) error {
-	id := c.Param("id")
-	// fmt.Println(id)
-	for _, m := range movies {
-		if m.ImdbID == id {
-			return c.JSON(200, m)
-		}
-	}
-	return c.JSON(200, map[string]string{"message": "movie" + id})
-}
-func createMovies(c echo.Context) error {
-	m := &Movie{}
-
-	if err := c.Bind(m); err != nil {
-		return c.JSON(400, err.Error())
-	}
-	movies = append(movies, *m)
-	return c.JSON(200, m)
-}
-
-// func getUser(c echo.Context) error {
-// 	// Add logic for getting a user by ID
-// 	return c.JSON(http.StatusOK, "User retrieved successfully")
-// }
-
-// func createUser(c echo.Context) error {
-// 	// Add logic for creating a new user
-// 	return c.JSON(http.StatusOK, "User created successfully")
-// }
-
-// func updateUser(c echo.Context) error {
-// 	// Add logic for updating a user by ID
-// 	return c.JSON(http.StatusOK, "User updated successfully")
-// }
-
-// func deleteUser(c echo.Context) error {
-// 	// Add logic for deleting a user by ID
-// 	return c.JSON(http.StatusOK, "User deleted successfully")
-// }
