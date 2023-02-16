dbcdf7821cbd699ac470318fbea730052557101e
farm rakpong <farmza560@gmail.com>
2023-02-16 10:32:26 +0700

    add movies

--- Controller/movies/movies.go
+++ Controller/movies/movies.go
@@ -1,4 +1,4 @@
-package product
+package movie
 
 import (
 	"strconv"
--- routes/Route.go
+++ routes/Route.go
@@ -1,19 +1,19 @@
 package routes
 
 import (
-	"github.com/farmrakpong/goimdbreal/controller/product"
+	movie "github.com/farmrakpong/goimdbreal/controller/movies"
 	"github.com/labstack/echo/v4"
 )
 
 func UserRoute(e *echo.Echo) {
 	// Define the route for getting all users
-	e.GET("/movies", product.GetAllMoviesHandler)
+	e.GET("/movies", movie.GetAllMoviesHandler)
 
 	// // Define the route for getting a user by ID
-	e.GET("/movies/:id", product.GetAllMoviesByIDHandlerByID)
+	e.GET("/movies/:id", movie.GetAllMoviesByIDHandlerByID)
 
 	// // Define the route for creating a new user
-	e.POST("/users", product.CreateMovies)
+	e.POST("/users", movie.CreateMovies)
 
 	// // Define the route for updating a user by ID
 	// e.PUT("/users/:id", updateUser)
